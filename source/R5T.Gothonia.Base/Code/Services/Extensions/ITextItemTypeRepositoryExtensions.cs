c20e94d064b3248e41aef68786661746439f5082
MinexAutomation <minex.automation@gmail.com>
2019-12-05 14:00:15 -0800

    Added Acquire() text-item type repository extension method.

--- source/R5T.Gothonia.Base/Code/Services/Extensions/ITextItemTypeRepositoryExtensions.cs
+++ source/R5T.Gothonia.Base/Code/Services/Extensions/ITextItemTypeRepositoryExtensions.cs
@@ -7,6 +7,20 @@ namespace R5T.Gothonia
 {
     public static class ITextItemTypeRepositoryExtensions
     {
+        public static TextItemType Acquire(this ITextItemTypeRepository repository, string name)
+        {
+            var exists = repository.Exists(name);
+
+            var identity = exists ? repository.GetIdentity(name) : repository.New(name);
+
+            var output = new TextItemType()
+            {
+                Identity = identity,
+                Name = name,
+            };
+            return output;
+        }
+
         public static bool Exists(this ITextItemTypeRepository repository, Guid identity)
         {
             var textItemTypeIdentity = TextItemTypeIdentity.From(identity);
