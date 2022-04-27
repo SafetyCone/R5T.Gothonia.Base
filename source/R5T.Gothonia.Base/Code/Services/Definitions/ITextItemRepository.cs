c7012247353608b01112af6f18264713df3f282d
davidcoats <david@getsafetycone.com>
2022-04-27 15:31:34 -0700

    Interim changes.

---
+++ source/R5T.Gothonia.Base/Code/Services/Definitions/ITextItemRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using R5T.Siscia;
+
+using R5T.T0064;
+
+
+namespace R5T.Gothonia
+{
+    [ServiceDefinitionMarker]
+    public interface ITextItemRepository : IServiceDefinition
+    {
+        Task<TextItemIdentity> New();
+        Task<TextItemIdentity> New(TextItemTypeIdentity typeIdentity, string value);
+
+        Task<TextItemIdentity> Add(TextItem textItem);
+
+        Task<bool> Exists(TextItemIdentity identity);
+
+        Task SetItemType(TextItemIdentity identity, TextItemTypeIdentity typeIdentity);
+        Task<TextItemTypeIdentity> GetItemType(TextItemIdentity identity);
+
+        Task SetValue(TextItemIdentity identity, string value);
+        Task<string> GetValue(TextItemIdentity identity);
+
+        Task Delete(TextItemIdentity identity);
+
+        Task<List<(string TypeName, string Value)>> GetTypedValuePairs(IEnumerable<TextItemIdentity> textItemIdentities);
+    }
+}
---
+++ source/R5T.Gothonia.Base/Code/Services/Definitions/ITextItemTypeRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.Siscia;
+
+using R5T.T0064;
+
+
+namespace R5T.Gothonia
+{
+    [ServiceDefinitionMarker]
+    public interface ITextItemTypeRepository : IServiceDefinition
+    {
+        Task<TextItemTypeIdentity> New();
+        Task<TextItemTypeIdentity> New(string name);
+
+        /// <summary>
+        /// Returns void to indicate that the identity of the input must be set.
+        /// </summary>
+        Task Add(TextItemType textItemType);
+
+        Task<bool> Exists(TextItemTypeIdentity identity);
+        Task<bool> Exists(string name);
+
+        Task<TextItemTypeIdentity> GetIdentity(string name);
+        Task<TextItemType> Get(string name);
+
+        Task<TextItemType> Get(TextItemTypeIdentity identity);
+
+        Task SetName(TextItemTypeIdentity identity, string name);
+        Task<string> GetName(TextItemTypeIdentity identity);
+
+        Task Delete(TextItemTypeIdentity identity);
+    }
+}
